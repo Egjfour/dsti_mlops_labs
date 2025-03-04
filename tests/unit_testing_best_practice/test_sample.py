3d4c970abc58cff3bff8469baeacf8256e97df87
Egjfour <ejenkins20@jcu.edu>
2025-03-04 14:39:44 +0100

    Unit Testing Simple Examples

---
+++ tests/unit_testing_best_practice/test_sample.py
@@ -0,0 +1,4 @@
+from src.unit_testing_best_practice.sample import func
+
+def test_func():
+    assert func(3) == 5
---
+++ tests/unit_testing_simple/test_sample.py
@@ -0,0 +1,7 @@
+# This is meant to be a very simple introduction to testing at a high level
+
+def func(x):
+    return x + 1
+
+def test_answer():
+    assert func(3) == 5
